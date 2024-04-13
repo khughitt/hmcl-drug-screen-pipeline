f63235b9a6965577d1e2afd754e0bb8fcd3fa543
Keith Hughitt <keith.hughitt@gmail.com>
2024-04-13 16:28:04 -0400

    fix row/col names for concatenated matrices

--- scripts/create_combined_response_matrices.R
+++ scripts/create_combined_response_matrices.R
@@ -1,9 +1,5 @@
 #
-# Creates reduced-dimension versions of the adjusted drug screen data with respect to drugs/cells
-#
-# in order to make use of all of the available data, instead of projecting the drug x cell matrices
-# for a single dose or the inferred AC-50 scores, viability measurements for all doses are
-# concatenated and used as input for the dimension reduction step.
+# Combines viability measurements for all doses by cell and drug and stores the resulting matrices.
 #
 library(tidyverse)
 
@@ -18,7 +14,9 @@ for (infile in infiles) {
   drug_mats[[dose]] <- read_tsv(infile, show_col_types = FALSE)
 }
 
+# store cell/drug ids
 cell_lines <- drug_mats[[1]]$cell_line
+drug_ids <- colnames(drug_mats[[1]])[-1]
 
 for (dose in names(drug_mats)) {
   drug_mats[[dose]] <- drug_mats[[dose]] %>%
@@ -26,20 +24,19 @@ for (dose in names(drug_mats)) {
     as.matrix()
 }
 
-# 1. cell-indexed response matrice
+# 1. cell-indexed response matrice;
 dat <- as.data.frame(do.call(cbind, drug_mats))
-write_tsv(dat, snakemake@output[[1]])
+rownames(dat) <- cell_lines
+colnames(dat) <- sprintf("X%d", seq_len(ncol(dat)))
+
+dat %>%
+  rownames_to_column("cell") %>%
+  write_tsv(snakemake@output[[1]])
 
 # 2. drug-indexed response matrice
 dat <- as.data.frame(t(do.call(rbind, drug_mats)))
-write_tsv(dat, snakemake@output[[2]])
+rownames(dat) <- drug_ids
 
-# pca <- prcomp(t(dat), scale = TRUE)
-#
-# pca_dat <- pca$x[, 1:3]
-# colnames(pca_dat) <- c("PC1", "PC2", "PC3")
-#
-# compute variance explained
-# var_explained <- summary(pca)$importance[2, 1:3] * 100
-
-
+dat %>%
+  rownames_to_column("drug_id") %>%
+  write_tsv(snakemake@output[[2]])
